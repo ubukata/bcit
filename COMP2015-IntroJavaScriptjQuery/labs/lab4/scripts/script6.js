c98bf83deadb2dde71129cec72d30022172b8843
Vitor Ubukata <vitor.ubukata@gmail.com>
2016-05-11 22:59:05 -0700

    dev script 6 - jquery ui tooltip

--- COMP2015-IntroJavaScriptjQuery/labs/lab4/scripts/script6.js
+++ COMP2015-IntroJavaScriptjQuery/labs/lab4/scripts/script6.js
@@ -1,2 +1,20 @@
 $(function(){
+	$( document ).tooltip({
+      items: "a",
+      content: function() {
+        var element = $( this );
+        if ( element.is( "a" ) && element.hasClass("tooltip") ) {
+			if(element.is("[data-reference]")){
+				var html = $("#" + element.data("reference")).html();
+				return html;
+			}
+			else if ( element.is( "[title]" ) ) {
+			  return element.attr( "title" );
+			}
+			else {
+			  return element.text();
+			}
+        }
+      }
+    });
 });
