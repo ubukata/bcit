a7f0d6c5597b487f4b9d69362ab72201be2b4a96
Vitor Ubukata <vitor.ubukata@gmail.com>
2016-06-27 00:57:06 -0700

    developmento final project web dev

---
+++ COMP1850-WebDevelopmentAndDesign1/final/scripts/main.js
@@ -0,0 +1,20 @@
+window.onload = function(e) {
+	"use strict"
+
+	var main = {
+		loadCodeMirror: function () {
+			var textAreas = document.getElementsByClassName("code-mirror");
+			var coreMirrorInstances = [];
+			for (var i = textAreas.length - 1; i >= 0; i--) {
+				coreMirrorInstances.push(CodeMirror.fromTextArea(textAreas[i], {
+				    mode: {name: "javascript", json: true},
+			        theme: "default",
+			        lineNumbers: true,
+			        readOnly: true
+				}));
+			}
+		}
+	};
+
+	main.loadCodeMirror();
+};
