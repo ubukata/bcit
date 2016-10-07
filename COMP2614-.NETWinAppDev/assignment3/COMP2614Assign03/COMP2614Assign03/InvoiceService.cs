316598b29113b4dc42a1321585f8ec466e5e7a9a
Vitor Ubukata <vitor.ubukata@gmail.com>
2016-10-06 22:02:20 -0700

    add assignment 3 .net

---
+++ COMP2614-.NETWinAppDev/assignment3/COMP2614Assign03/COMP2614Assign03/InvoiceService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2614Assign03
+{
+    public class InvoiceService
+    {
+        private List<Invoice> Invoices;
+
+        private const decimal Gst = 0.05m;
+        private const decimal Pst = 0.07m;
+
+        protected InvoiceService()
+        {
+            Invoices = new List<Invoice>();
+        }
+
+        public InvoiceService(List<string> fileData) : this()
+        {
+            ParseFile(fileData);
+        }
+
+        private void ParseFile(List<string> fileData)
+        {
+            Invoice invoice = null;
+
+            foreach (string data in fileData)
+            {
+                string[] splitItems = data.Split('|');
+                string header = splitItems[0];
+                string[] headerSplit = header.Split(':');
+
+                invoice = new Invoice(headerSplit);
+
+                for (int i = 1; i < splitItems.Length; i++)
+                {
+                    invoice.AddItem(splitItems[i].Split(':'));
+                }
+
+                Invoices.Add(invoice);
+            }
+        }
+
+        public IEnumerable<Invoice> GetInvoices()
+        {
+            return Invoices;
+        }
+    }
+
+    public class Invoice
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public char[] Terms { get; set; }
+        public List<InvoiceItem> Items { get; set; }
+
+        public Invoice()
+        {
+            Items = new List<InvoiceItem>();
+        }
+
+        public Invoice(string[] data) : this()
+        {
+            Number = Convert.ToInt32(data[0]);
+            Date = Convert.ToDateTime(data[1]);
+            Terms = data[2].ToCharArray();
+        }
+
+        public void AddItem(string[] data)
+        {
+            InvoiceItem newItem = new InvoiceItem(data);
+            Items.Add(newItem);
+        }
+    }
+
+    public class InvoiceItem
+    {
+
+        public int Quantity { get; set; }
+        public string Sku { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public char Taxable { get; set; }
+
+        public decimal ExtendedCost
+        {
+            get
+            {
+                return Quantity * Price;
+            }
+        }
+
+        public InvoiceItem()
+        {
+
+        }
+
+        public InvoiceItem(string[] data)
+        {
+            Quantity = Convert.ToInt32(data[0]);
+            Sku = data[1];
+            Description = data[2];
+            Price = Convert.ToDecimal(data[3]);
+            Taxable = data[4][0];
+        }
+    }
+}
---
+++ COMP2614-.NETWinAppDev/assignment3/COMP2614Assign03/COMP2614Assign03/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2614Assign03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                return;
+
+            string filePath = args[0];
+
+            List<string> fileData = ReadFile(filePath);
+
+            InvoiceService invoiceService = new InvoiceService(fileData);
+            IEnumerable<Invoice> invoices = invoiceService.GetInvoices();
+
+            ConsolePrinter.PrintInvoices(invoices);
+
+        }
+
+        static List<string> ReadFile(string path)
+        {
+            List<string> data = new List<string>();
+
+            if (File.Exists(path))
+            {
+                StreamReader reader = null;
+                try
+                {
+                    reader = new StreamReader(path);
+                    while(reader.Peek() > 0)
+                    {
+                        data.Add(reader.ReadLine());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\n{ex.Message}\n");
+                }
+                finally
+                {
+                    if(reader != null)
+                    {
+                        reader.Dispose();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nFile not found.\n");
+            }
+
+            return data;
+        }
+    }
+
+    public class ConsolePrinter
+    {
+        public static void PrintInvoices(IEnumerable<Invoice> invoices)
+        {
+            Console.WriteLine("Invoice Listing");
+            PrintLineSeparator();
+            Console.WriteLine();
+
+            foreach (Invoice invoice in invoices)
+            {
+                PrintInvoice(invoice);
+            }
+        }
+
+        private static void PrintInvoice(Invoice invoice)
+        {
+            Console.WriteLine($"{"Invoice Number:", 15} {invoice.Number}");
+            Console.WriteLine($"{"Invoice Date:",15} {invoice.Date.ToLongDateString()}");
+            Console.WriteLine($"{"Discount Date:",15} {invoice.Date.ToLongDateString()}");
+            Console.WriteLine($"{"Terms:",15} {invoice.Terms[0]}% {invoice.Terms[1]}{invoice.Terms[2]} days ADI");
+            PrintLineSeparator();
+            Console.WriteLine("{Qty, 3} {SKU, 8} {Description, 20} {Price, 7} {PST, 3} Ext");
+            PrintLineSeparator();
+            decimal subTotal = 0;
+            decimal tax = 0;
+            foreach (InvoiceItem item in invoice.Items)
+            {
+                Console.WriteLine($"{item.Quantity, 3} {item.Sku, 8} {item.Description, 20} {item.Price, 7:N2} {item.Taxable, 3} {item.ExtendedCost}");
+                subTotal += item.ExtendedCost;
+            }
+            decimal total = subTotal + tax;
+            decimal discount = total * ((int)char.GetNumericValue(invoice.Terms[0]) / 100);
+            PrintLineSeparator();
+            Console.WriteLine($"{"",12} {"Subtotal:",20} {"",11} {subTotal}");
+            Console.WriteLine($"{"",12} {"GST:",20} {"",11} {tax}");
+            PrintLineSeparator();
+            Console.WriteLine($"{"",12} {"Total:",20} {"",11} {total}");
+            Console.WriteLine();
+            Console.WriteLine($"{"",12} {"Discount:",20} {"",11} {discount}");
+        }
+
+        private static void PrintLineSeparator()
+        {
+            Console.WriteLine("-------------------------------");
+        }
+    }
+}
